dccc997b32fa5a8569582ee966e0cc9b243a3065
MiguelF <miguel.barbosa.domingues@gmail.com>
2025-09-29 23:15:06 -0300

    'aula-expensive'

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -6,36 +6,46 @@ class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
       return FirebaseOptions(
-        apiKey: "AIzaSyD54umzX4ksDFHRSBSa-EK66vmDCztGa3s",
-        authDomain: "financing-app-4c4d5.firebaseapp.com",
-        projectId: "financing-app-4c4d5",
-        storageBucket: "financing-app-4c4d5.firebasestorage.app",
-        messagingSenderId: "891273066809",
-        appId: "1:891273066809:web:6ee62718d1bf22445810e8",
+        apiKey: "AIzaSyDOqD1x2LkrACyxWp_KLm1avWFfeOU3XZk",
+        authDomain: "financing-app-97aed.firebaseapp.com",
+        projectId: "financing-app-97aed",
+        storageBucket: "financing-app-97aed.firebasestorage.app",
+        messagingSenderId: "128092159591",
+        appId: "1:128092159591:web:14da48b411480413d41ec2",
+        measurementId: "G-E91C925XL3"
       );
     }
 
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return FirebaseOptions(
-          apiKey: 'SUA API KEY',
-          appId: 'SEU APP ID',
-          messagingSenderId: 'SEU MESSAGE ID',
-          projectId: 'SEU PROJECT ID',
+        apiKey: "AIzaSyDOqD1x2LkrACyxWp_KLm1avWFfeOU3XZk",
+        authDomain: "financing-app-97aed.firebaseapp.com",
+        projectId: "financing-app-97aed",
+        storageBucket: "financing-app-97aed.firebasestorage.app",
+        messagingSenderId: "128092159591",
+        appId: "1:128092159591:web:14da48b411480413d41ec2",
+        measurementId: "G-E91C925XL3"
         );
       case TargetPlatform.iOS:
         return FirebaseOptions(
-          apiKey: 'SUA API KEY',
-          appId: 'SEU APP ID',
-          messagingSenderId: 'SEU MESSAGE ID',
-          projectId: 'SEU PROJECT ID',
+        apiKey: "AIzaSyDOqD1x2LkrACyxWp_KLm1avWFfeOU3XZk",
+        authDomain: "financing-app-97aed.firebaseapp.com",
+        projectId: "financing-app-97aed",
+        storageBucket: "financing-app-97aed.firebasestorage.app",
+        messagingSenderId: "128092159591",
+        appId: "1:128092159591:web:14da48b411480413d41ec2",
+        measurementId: "G-E91C925XL3"
         );
       default: 
         return FirebaseOptions (
-          apiKey: 'SUA API KEY',
-          appId: 'SEU APP ID',
-          messagingSenderId: 'SEU MESSAGE ID',
-          projectId: 'SEU PROJECT ID',
+        apiKey: "AIzaSyDOqD1x2LkrACyxWp_KLm1avWFfeOU3XZk",
+        authDomain: "financing-app-97aed.firebaseapp.com",
+        projectId: "financing-app-97aed",
+        storageBucket: "financing-app-97aed.firebasestorage.app",
+        messagingSenderId: "128092159591",
+        appId: "1:128092159591:web:14da48b411480413d41ec2",
+        measurementId: "G-E91C925XL3"
       );
     }
   }
